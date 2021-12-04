19c17248391602b79d71de8a989f69c49a4f3928
manuferreira <rmanussa@outlook.com>
2021-12-04 17:45:46 -0300

    accountoptions

--- lib/View/accountOptions.dart
+++ lib/View/accountOptions.dart
@@ -2,48 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(AccountOptions(title: 'title'));
+  runApp(AccountOptions());
 }
 
 class AccountOptions extends StatefulWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp (
-        home: AccountOptions(title: 'title')
-    );
-  }
-  const AccountOptions({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const AccountOptions({Key? key}) : super(key: key);
+  //final String title;
 
   @override
   _AccountOptionsState createState() => _AccountOptionsState();
-
 }
 
 class _AccountOptionsState extends State<AccountOptions> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title), centerTitle: true,),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: SvgPicture.asset('assets/undraw_personal_information_re_vw8a.svg'),
-              width: 250,
-              height: 250,
-              margin: EdgeInsets.only(bottom: 10),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  FloatingActionButton.extended(
-                      onPressed: () {Navigator.pushNamed(context, '/');},
-                      label: Text('Editar perfil'))
-                ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('What2Read'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: SvgPicture.asset(
+                    'assets/undraw_personal_information_re_vw8a.svg'),
+                width: 250,
+                height: 250,
+                margin: EdgeInsets.only(bottom: 10, top: 20),
               ),
-            )
-          ],
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 190,
+                      height: 50,
+                      child: FloatingActionButton.extended(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/');},
+                          label: Text('Editar perfil')),
+                        margin: EdgeInsets.only(bottom: 20, top: 20)
+                    ),
+                    Container(
+                      width: 190,
+                      height: 50,
+                      child: FloatingActionButton.extended(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/');},
+                          label: Text('Excluir conta')),
+                        margin: EdgeInsets.only(bottom: 20)
+                    ),
+                    Container (
+                      width: 190,
+                      height: 50,
+                      child: FloatingActionButton.extended(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/');
+                          },
+                          label: Text('Sair')) ,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
